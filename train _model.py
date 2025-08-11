5c98ebbc932d4f76c69a262bc9615823ab61e047
Krrish Rathor <149479786+krrishrathor@users.noreply.github.com>
2025-08-12 00:06:08 +0530

    Update train _model.py
    "dataset fetched"

--- train _model.py
+++ train _model.py
@@ -9,3 +9,14 @@ from PIL import Image #open iamges for prediction
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f" Using device: {device}")
 
+DATA_DIR = "asl_alphabet_train"   # Folder containing asl dataset
+BATCH_SIZE = 32 # Batch size for training
+EPOCHS = 10
+MODEL_PATH = "model.pth" # Path to save the trained model
+CLASSES_FILE = "classes.txt"#A text file to store all ASL class names for later.
+
+transform = transforms.Compose([
+    transforms.Resize((64, 64)), #bring evry image to 64x64
+    transforms.ToTensor(), # Converts images to PyTorch tensors ([0,1] range).
+    transforms.Normalize([0.5], [0.5]) # Normalize images to [-1, 1] range.
+])
